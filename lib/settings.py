69bee99fafbc76147e85f5fcb80db36300e425dd
vladyusenko77 <47138991+vladyusenko77@users.noreply.github.com>
2020-04-29 12:33:26 +0300

    u

--- lib/settings.py
+++ lib/settings.py
@@ -1,5 +1,5 @@
 token = "1142079773:AAHLjpko8HY8f_IvIi3IrZAX6s88oJRacfg"
-host = "104.211.13.232"
+host = "104.211.0.221"
 user = "root"
-passwd = "my-secret-root"
+passwd = "my-secret-pass"
 database = ""
