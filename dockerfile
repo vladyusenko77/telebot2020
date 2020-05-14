71f86e61702d2e5845c4841186749238e512a4ea
vladyusenko77 <47138991+vladyusenko77@users.noreply.github.com>
2020-05-14 13:02:35 +0300

    docker-compose

--- dockerfile
+++ dockerfile
@@ -1,5 +1,17 @@
 FROM python:3
+
 WORKDIR /usr/src/app
+
+ENV DB_HOST=localhost
+ENV DB_USER=root
+ENV DB_PASSWORD=root
+ENV TELEGRAM_TOKEN=00000000000000000000000
+# ENV env_var_host=$DB_HOST
+# ENV env_var_user=$DB_USER
+# ENV env_var_password=$DB_PASSWORD
+# ENV env_var_token=$TELEGRAM_TOKEN
 COPY . /usr/src/app
 RUN pip install --no-cache-dir -r requirements.txt
+
+
 CMD [ "python", "./start.py" ]
--- lib/settings.py
+++ lib/settings.py
@@ -1,4 +1,9 @@
-token = "1142079773:AAHLjpko8HY8f_IvIi3IrZAX6s88oJRacfg"
-host = "13.53.36.22"
-user = "root"
-passwd = "my-secret-pass"
+import os
+host = os.environ['DB_HOST']
+user = os.environ['DB_USER']
+passwd = os.environ['DB_PASSWORD']
+token = os.environ['TELEGRAM_TOKEN']
+# token = "1142079773:AAHLjpko8HY8f_IvIi3IrZAX6s88oJRacfg"
+# host = "13.49.21.219"
+# user = "root"
+# passwd = "my-secret-pass"
